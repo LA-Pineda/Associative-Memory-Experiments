1ad00838348472191f1aba1a9946928cdabe1da7
Rafael Morales-Gamboa <rmorales@suv.udg.mx>
2020-08-12 14:25:52 -0500

    Added comments to mnist.py, to make it easier (for me) to understand what it does.

--- mnist.py
+++ mnist.py
@@ -8,22 +8,34 @@ import numpy as np
 
 
 def load_mnist(mnist_path, n_train=60000, n_test=10000, onehot=True):
+    """ Loads training and testing data from NMIST
+
+        It loads all data, normalizes it, converts the columns of labels
+        in a binary matrix, and finally returns the amount of data
+        requested.
+    """
+
+    # Reads full training data: 60,000 images of 28x28 bytes.
     fd = open(os.path.join(mnist_path, 'train-images-idx3-ubyte'))
     loaded = np.fromfile(file=fd, dtype=np.uint8)
     trX = loaded[16:].reshape((60000, 28 * 28)).astype(np.float32)
 
+    # Reads full training labels.
     fd = open(os.path.join(mnist_path, 'train-labels-idx1-ubyte'))
     loaded = np.fromfile(file=fd, dtype=np.uint8)
     trY = loaded[8:].reshape((60000, ))
 
+    # Reads full testing data: 10,000 images of 28x28 bytes.
     fd = open(os.path.join(mnist_path, 't10k-images-idx3-ubyte'))
     loaded = np.fromfile(file=fd, dtype=np.uint8)
     teX = loaded[16:].reshape((10000, 28 * 28)).astype(np.float32)
 
+    # Reads full testing labels.
     fd = open(os.path.join(mnist_path, 't10k-labels-idx1-ubyte'))
     loaded = np.fromfile(file=fd, dtype=np.uint8)
     teY = loaded[8:].reshape((10000, ))
 
+    # Normalizes values in range(256)
     trX /= 255.0
     teX /= 255.0
 
@@ -34,6 +46,8 @@ def load_mnist(mnist_path, n_train=60000, n_test=10000, onehot=True):
     teY = teY[:n_test]
 
     if onehot:
+        # Converts a column of values in range(10) into a matrix where
+        # [i, value] = 1, and otherwise [i,j] = 0.
         trY = one_hot(trY, 10)
         teY = one_hot(teY, 10)
     else:
@@ -44,6 +58,11 @@ def load_mnist(mnist_path, n_train=60000, n_test=10000, onehot=True):
 
 
 def one_hot(x, n):
+    """ Transforms an column of values in a binary matrix.
+
+        From a column of m values in range(n), it produces a binary matrix
+        of size m x n, where entry at [k,k_value] = 1, and otherwise [k,j] = 0
+    """
     if type(x) == list:
         x = np.array(x)
     x = x.flatten()
