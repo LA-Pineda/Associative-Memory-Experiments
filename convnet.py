f4f2e808103781f75eaabe73276991abb4ebbc40
Rafael Morales-Gamboa <rmorales@suv.udg.mx>
2020-08-15 19:22:00 -0500

    Changed neural net implementation, from Theano to TensorFlow. The model is trained in the classical way (batches of training data and evalution on testing data.

--- convnet.py
+++ convnet.py
@@ -1,75 +1,93 @@
-#!/usr/bin/env python
-
-# Based on Alec Radford code (https://github.com/Newmu/Theano-Tutorials)
-# Created by Raul Peralta-Lozada
-
-import theano
-
 import numpy as np
-from theano import tensor as T
-from theano.sandbox.rng_mrg import MRG_RandomStreams as RandomStreams
-from theano.tensor.nnet import conv2d
-from theano.tensor.signal.pool import pool_2d
+import tensorflow as tf
+from tensorflow.keras.utils import to_categorical
 
 
-srng = RandomStreams()
+import constants
 
 
-def floatX(X):
-    return np.asarray(X, dtype=theano.config.floatX)
+EPOCHS = 5
 
 
-def init_weights(shape):
-    return theano.shared(floatX(np.random.randn(*shape) * 0.01))
+def get_data(one_hot = False):
+
+   # Load MNIST data, as part of TensorFlow.
+    mnist = tf.keras.datasets.mnist
+    (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+
+    # MNIST dataset includes 60,000 training elements.
+    train_images = train_images.reshape((60000, 28, 28, 1))
+    train_images = train_images.astype('float32') / 255
+
+    # MNIST dataset includes 10,000 testing elements.
+    test_images = test_images.reshape((10000, 28, 28, 1))
+    test_images = test_images.astype('float32') / 255
+
+    if one_hot:
+        # Changes labels to binary rows. Each label correspond to a column, and only
+        # the column for the corresponding label is set to one.
+        train_labels = to_categorical(train_labels)
+        test_labels = to_categorical(test_labels)
+
+    return (train_images, train_labels), (test_images, test_labels)
 
 
-def rectify(X):
-    return T.maximum(X, 0.)
+def train_network():
+
+    (train_images, train_labels), (test_images, test_labels) = get_data(one_hot= True)
+
+    # Creates a two parts neural network: a convolutional neural network (CNN) with three
+    # main layers, and a full connected neural network.
+    model = tf.keras.models.Sequential()
+    model.add(tf.keras.layers.Conv2D(32,(3, 3), activation='relu', padding='same', input_shape=(28, 28, 1)))
+    model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+    model.add(tf.keras.layers.Dropout(0.2)),
+    model.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+    model.add(tf.keras.layers.Dropout(0.2)),
+    model.add(tf.keras.layers.Conv2D(32, (3, 3), activation='relu'))
+    model.add(tf.keras.layers.Flatten())
+    # model.add(tf.keras.layers.Dropout(0.2)),
+    model.add(tf.keras.layers.Dense(64, activation='relu'))
+    model.add(tf.keras.layers.Dense(10, activation='softmax'))
+    
+    model.summary()
+
+    model.compile(loss='categorical_crossentropy',
+                optimizer='sgd',
+                metrics=['accuracy'])
+
+    model.fit(train_images, train_labels,
+            batch_size=100,
+            epochs=EPOCHS,
+            verbose=1)
+
+    test_loss, test_acc = model.evaluate(test_images, test_labels)
+
+    print('Test accuracy:', test_acc)
+
+    model.save(constants.full_model_filename)
 
 
-def softmax(X):
-    e_x = T.exp(X - X.max(axis=1).dimshuffle(0, 'x'))
-    return e_x / e_x.sum(axis=1).dimshuffle(0, 'x')
+def obtain_features():
 
+    (train_images, train_labels), (test_images, test_labels) = get_data()
 
-def dropout(X, p=0.0):
-    if p > 0:
-        retain_prob = 1 - p
-        X *= srng.binomial(X.shape, p=retain_prob, dtype=theano.config.floatX)
-        X /= retain_prob
-    return X
+    # Recreate the exact same model, including its weights and the optimizer
+    model = tf.keras.models.load_model(constants.full_model_filename)
 
+    # Drop the full connected neural network part.
+    model.pop()
+    model.pop()
 
-def RMSprop(cost, params, lr=0.001, rho=0.9, epsilon=1e-6):
-    grads = T.grad(cost=cost, wrt=params)
-    updates = []
-    for p, g in zip(params, grads):
-        acc = theano.shared(p.get_value() * 0.)
-        acc_new = rho * acc + (1 - rho) * g ** 2
-        gradient_scaling = T.sqrt(acc_new + epsilon)
-        g = g / gradient_scaling
-        updates.append((acc, acc_new))
-        updates.append((p, p - lr * g))
-    return updates
+    model.summary()
 
+    # 'Do not specify the batch_size if your data is in the form of dataset...
+    # (since they generate batches).
+    features = model.predict(train_images)
+    np.save(constants.train_features_filename, features)
+    
+    features = model.predict(test_images)
+    np.save(constants.test_features_filename, features)
 
-def convnet_model(X, w1, w2, w3, w4, w5, p_drop_conv, p_drop_hidden):
-    # l1_relu = rectify(conv2d(X, w1, border_mode='full'))
-    l1_relu = T.nnet.relu(conv2d(X, w1, border_mode='full'), alpha=0)
-    l1_pool = pool_2d(l1_relu, (2, 2), ignore_border=False)
-    l1_out = dropout(l1_pool, p_drop_conv)
-
-    l2_relu = T.nnet.relu(conv2d(l1_out, w2))
-    l2_pool = pool_2d(l2_relu, (2, 2), ignore_border=False)
-    l2_out = dropout(l2_pool, p_drop_conv)
-
-    l3_relu = T.nnet.relu(conv2d(l2_out, w3))
-    l3_pool = pool_2d(l3_relu, (2, 2), ignore_border=False)
-    l3_flat = T.flatten(l3_pool, outdim=2)
-    l3_out = dropout(l3_flat, p_drop_conv)
-
-    l4_relu = T.nnet.relu(T.dot(l3_out, w4))
-    l4_out = dropout(l4_relu, p_drop_hidden)
-
-    pyx = softmax(T.dot(l4_out, w5))
-    return l1_out, l2_out, l3_out, l4_out, pyx
+    model.save(constants.features_model_filename)
